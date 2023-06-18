093ec67c01aa8740c3380c01c17d777031291533
qinye233“$ git config --global user.namgit config --global user.name qinye233“ <Microas@foxmail.com>
2023-06-18 21:01:01 +0800

    debounce

---
+++ JS/Javascript高级语法学习/21.防抖节流函数/02_防抖函数_this和event绑定.js
@@ -0,0 +1,18 @@
+function qbDebounce(fn, delay) {
+    // 用于记录上一次事件触发的timer
+  let timer = null;
+    // 事件触发执行的函数
+  const _debounce = function (...args) {
+    if (timer) {
+        // 如果有再次触发事件,那么取消上一次的事件
+      clearTimeout(timer);
+    }
+    // 延迟去执行对应的fn函数(传入的回调函数)
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+      timer = null; //指向函数后.将timer重置为null
+    }, delay);
+  };
+
+  return _debounce;
+}
---
+++ JS/Javascript高级语法学习/21.防抖节流函数/03_防抖函数_取消功能.js
@@ -0,0 +1,26 @@
+function qbDebounce(fn, delay) {
+  // 用于记录上一次事件触发的timer
+  let timer = null;
+  // 事件触发执行的函数
+  const _debounce = function (...args) {
+    if (timer) {
+      // 如果有再次触发事件,那么取消上一次的事件
+      clearTimeout(timer);
+    }
+    // 延迟去执行对应的fn函数(传入的回调函数)
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+      timer = null; //指向函数后.将timer重置为null
+    }, delay);
+  };
+
+  //函数是一个一个对象,所以我们给它添加属性
+  //给debounce添加一个取消功能
+  _debounce.cancel = function () {
+    if (timer) {
+      clearTimeout(timer);
+      console.log("请求已被取消");
+    }
+  };
+  return _debounce;
+}
